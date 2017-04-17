fab1b778747fda77b498ca3a1998c7e0171c1ae8
Stephen Long <long2know@gmail.com>
2017-04-17 15:55:40 -0400

    Initial code drop

---
+++ aspnetcore-angular/Client/app/app.module.ts
@@ -0,0 +1,146 @@
+﻿import { NgModule, Inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule, APP_BASE_HREF } from '@angular/common';
+import { HttpModule, Http } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { Ng2BootstrapModule } from 'ng2-bootstrap';
+
+// i18n support
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { AppComponent } from './app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './containers/home/home.component';
+import { UsersComponent } from './containers/users/users.component';
+import { CounterComponent } from './containers/counter/counter.component';
+import { ChatComponent } from './containers/chat/chat.component';
+import { Ng2BootstrapComponent } from './containers/ng2-bootstrap-demo/ng2bootstrap.component';
+
+import { LinkService } from './shared/link.service';
+import { UserService } from './shared/user.service';
+import { ConnectionResolver } from './shared/route.resolver';
+import { ORIGIN_URL } from './shared/constants/baseurl.constants';
+import { TransferHttpModule } from '../modules/transfer-http/transfer-http.module';
+
+export function createTranslateLoader(http: Http, baseHref) {
+    // Temporary Azure hack
+    if (baseHref === null && typeof window !== 'undefined') {
+        baseHref = window.location.origin;
+    }
+    // i18n files are in `wwwroot/assets/`
+    return new TranslateHttpLoader(http, `${baseHref}/assets/i18n/`, '.json');
+}
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        UsersComponent,
+        HomeComponent,
+        ChatComponent,
+        Ng2BootstrapComponent
+    ], 
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        Ng2BootstrapModule.forRoot(), // You could also split this up if you don't want the Entire Module imported
+
+        TransferHttpModule, // Our Http TransferData method
+
+        // i18n support
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: (createTranslateLoader),
+                deps: [Http, [ORIGIN_URL]]
+            }
+        }),
+
+        // App Routing
+        RouterModule.forRoot([
+            {
+                path: '',
+                redirectTo: 'home',
+                pathMatch: 'full'
+            },
+            {
+                path: 'home', component: HomeComponent,
+
+                // *** SEO Magic ***
+                // We're using "data" in our Routes to pass in our <title> <meta> <link> tag information
+                // Note: This is only happening for ROOT level Routes, you'd have to add some additional logic if you wanted this for Child level routing
+                // When you change Routes it will automatically append these to your document for you on the Server-side
+                //  - check out app.component.ts to see how it's doing this
+                data: {
+                    title: 'Homepage',
+                    meta: [{ name: 'description', content: 'This is an example Description Meta tag!' }],
+                    links: [
+                        { rel: 'canonical', href: 'http://blogs.example.com/blah/nice' },
+                        { rel: 'alternate', hreflang: 'es', href: 'http://es.example.com/' }
+                    ]
+                }
+            },
+            {
+                path: 'counter', component: CounterComponent,
+                data: {
+                    title: 'Counter',
+                    meta: [{ name: 'description', content: 'This is an Counter page Description!' }],
+                    links: [
+                        { rel: 'canonical', href: 'http://blogs.example.com/counter/something' },
+                        { rel: 'alternate', hreflang: 'es', href: 'http://es.example.com/counter' }
+                    ]
+                }
+            },
+            {
+                path: 'users', component: UsersComponent,
+                data: {
+                    title: 'Users REST example',
+                    meta: [{ name: 'description', content: 'This is User REST API example page Description!' }],
+                    links: [
+                        { rel: 'canonical', href: 'http://blogs.example.com/chat/something' },
+                        { rel: 'alternate', hreflang: 'es', href: 'http://es.example.com/users' }
+                    ]
+                }
+            },
+            {
+                path: 'chat', component: ChatComponent,
+                // Wait until the resolve is finished before loading the Route
+                resolve: { connection: ConnectionResolver },
+                data: {
+                    title: 'SignalR chat example',
+                    meta: [{ name: 'description', content: 'This is an Chat page Description!' }],
+                    links: [
+                        { rel: 'canonical', href: 'http://blogs.example.com/chat/something' },
+                        { rel: 'alternate', hreflang: 'es', href: 'http://es.example.com/chat' }
+                    ]
+                }
+            },
+            {
+                path: 'ng2-bootstrap', component: Ng2BootstrapComponent,
+                data: {
+                    title: 'Ng2-bootstrap demo!!',
+                    meta: [{ name: 'description', content: 'This is an Demo Bootstrap page Description!' }],
+                    links: [
+                        { rel: 'canonical', href: 'http://blogs.example.com/bootstrap/something' },
+                        { rel: 'alternate', hreflang: 'es', href: 'http://es.example.com/bootstrap-demo' }
+                    ]
+                }
+            },
+
+            // All else fails - go home!
+            { path: '**', redirectTo: 'home' }
+        ])
+    ],
+    providers: [
+        LinkService,
+        UserService,
+        ConnectionResolver,
+        TranslateModule
+    ]
+})
+export class AppModule {
+}
---
+++ aspnetcore-angular/Client/app/containers/users/users.component.ts
@@ -0,0 +1,78 @@
+﻿import {
+    Component, OnInit,
+    // animation imports
+    trigger, state, style, transition, animate, Inject
+} from '@angular/core';
+import { IUser } from '../../models/User';
+import { UserService } from '../../shared/user.service';
+
+@Component({
+    selector: 'fetchdata',
+    templateUrl: './users.component.html',
+    animations: [
+        // Animation example
+        // Triggered in the ngFor with [@flyInOut]
+        trigger('flyInOut', [
+            state('in', style({ transform: 'translateY(0)' })),
+            transition('void => *', [
+                style({ transform: 'translateY(-100%)' }),
+                animate(1000)
+            ]),
+            transition('* => void', [
+                animate(1000, style({ transform: 'translateY(100%)' }))
+            ])
+        ])
+    ]
+})
+export class UsersComponent implements OnInit {
+
+    public newUserName: string;
+    public users: IUser[];
+
+    // Use "constructor"s only for dependency injection
+    constructor(private userService: UserService) { }
+
+    // Here you want to handle anything with @Input()'s @Output()'s
+    // Data retrieval / etc - this is when the Component is "ready" and wired up
+    ngOnInit() {
+        this.newUserName = '';
+        this.userService.getUsers().subscribe(result => {
+            console.log('Get user result: ', result);
+            console.log('TransferHttp [GET] /api/users/allresult', result);
+            this.users = result as IUser[];
+        });
+    }
+
+    deleteUser(user) {
+        this.userService.deleteUser(user).subscribe(result => {
+            console.log('Delete user result: ', result);
+            if (result.ok) {
+                let position = this.users.indexOf(user);
+                this.users.splice(position, 1);
+            } else {
+                alert('There was an issue, Could not delete user');
+            }
+        });
+    }
+
+    updateUser(user) {
+        this.userService.updateUser(user).subscribe(result => {
+            console.log('Put user result: ', result);
+            if (!result.ok) {
+                alert('There was an issue, Could not edit user');
+            }
+        });
+    }
+
+    addUser(newUserName) {
+        this.userService.addUser(newUserName).subscribe(result => {
+            console.log('Post user result: ', result);
+            if (result.ok) {
+                this.users.push(result.json());
+                this.newUserName = '';
+            } else {
+                alert('There was an issue, Could not edit user');
+            }
+        });
+    }
+}
